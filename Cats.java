618b1ab4b3fe6763d62b63f951360e2289408835
104615092022Vlad <113585901+104615092022Vlad@users.noreply.github.com>
2023-01-24 10:01:09 +0300

    Update Cats.java

--- Cats.java
+++ Cats.java
@@ -1,1 +1,15 @@
+import java.util.Random;
+public class Cats extends Animal{
+    Random r = new Random();
+    //лохматость
+    private boolean shaggy = r.nextBoolean();
+
+    public boolean getShaggy() {
+        return shaggy;
+    }
+
+    public void setShaggy(boolean shaggy) {
+        this.shaggy = shaggy;
+    }
+}
 
