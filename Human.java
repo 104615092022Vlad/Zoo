24fb4e007f59ffff8cf69f2f7775eedf4fcac237
104615092022Vlad <113585901+104615092022Vlad@users.noreply.github.com>
2023-01-24 09:55:39 +0300

    Update Human.java
    check & comment

--- Human.java
+++ Human.java
@@ -1,1 +1,28 @@
+public class Human {
+    //наименование должности сотрудника зоопарка
+    private String nameHuman = "Человек";
 
+    public String getNameHuman() {
+        return nameHuman;
+    }
+
+    public void setNameHuman(String nameHuman) {
+        this.nameHuman = nameHuman;
+    }
+
+    //слежение за белкой Стрелкой
+    public void watch() {
+        System.out.println("Человек следит за белкой Стрелкой");
+    }
+
+    //получение укуса от животного
+    private boolean injure;
+
+    public boolean getInjure() {
+        return injure;
+    }
+
+    public void setInjure(boolean injure) {
+        this.injure = injure;
+    }
+}
