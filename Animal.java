5e6b4e38c12c0dccfd31c548cbadeb1a273d7fd1
104615092022Vlad <113585901+104615092022Vlad@users.noreply.github.com>
2023-01-26 21:39:31 +0300

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,1 +1,63 @@
+import java.util.Random;
+public class Animal {
+    Random r = new Random();
+
+    //имя животного
+    private String name = "Животное";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //способность кусаться
+    public void bite(Human human, Animal animal) {
+        human.setInjure(r.nextBoolean());
+        if (human.getInjure() == true) {
+            System.out.println(human.getNameHuman() + " укушен");
+        } else {
+            System.out.println("Животное " + animal.getName() + " спокойно");
+        }
+    }
+
+    //свойство быть больным
+    private boolean sick = r.nextBoolean();
+
+    public boolean getSick() {
+        return sick;
+    }
+
+    public void setSick(boolean sick) {
+        this.sick = sick;
+    }
+
+    //пребываение в карантине
+    private boolean toBeIsolated = r.nextBoolean();
+
+    public boolean getToBeIsolated() {
+        return toBeIsolated;
+    }
+
+    public void setToBeIsolated(boolean isolation) {
+        this.toBeIsolated = isolation;
+    }
+
+    //сытость
+    private boolean satiety = r.nextBoolean();
+
+    public boolean getSatiety() {
+        return satiety;
+    }
+
+    public void setSatiety(boolean satiety) {
+        this.satiety = satiety;
+    }
+
+    public Animal(String name) {
+        this.name = name;
+    }
+}
 
