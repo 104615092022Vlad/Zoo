56b272dc852aa8726ad4ac23c136d7c8fc8b15ad
104615092022Vlad <113585901+104615092022Vlad@users.noreply.github.com>
2023-01-26 21:42:36 +0300

    Update Squirrel.java

--- Squirrel.java
+++ Squirrel.java
@@ -1,1 +1,30 @@
+import java.util.Random;
+public class Squirrel extends Animal{
+    Random r = new Random();
 
+    //свойство покидать зоопарк
+    private boolean escape = r.nextBoolean();
+
+    public boolean getEscape() {
+        return escape;
+    }
+
+    public void setEscape(boolean escape) {
+        this.escape = escape;
+    }
+
+    //свойство быть обнаруженной в случае побега
+    private boolean find = r.nextBoolean();
+
+    public boolean getFind() {
+        return find;
+    }
+
+    public void setFind(boolean find) {
+        this.find = find;
+    }
+
+    public Squirrel(String name) {
+        super(name);
+    }
+}
